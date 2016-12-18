3416eb3544205851a9a364c93f5f369ce702871b
phantom56 <phantom56@bk.ru>
2016-12-18 16:30:11 +0300

    jump jivo

--- assets/script.js
+++ assets/script.js
@@ -69,17 +69,19 @@ window.onload = function(){
   showJivoChat = function(){
     var jivo = document.getElementById('jivo-iframe-container');
     var count = 0;
-    if(jivo)var timerId1 = setTimeout(function (){
-      if(jivo.style.bottom == '0')
+    if(jivo)var timerId1 = setInterval(function (){
+      if(jivo.style.bottom == '0px')
         jivo.style.bottom = '20px';
       else if(jivo.style.bottom == '20px')
-        jivo.style.bottom = '0';
-      if(count == 4)
+        jivo.style.bottom = '0px';
+      if(count == 3){
+        clearInterval(timerId1);
         return;
+      }
       count++;
-    clearTimeout(timerId1);
+      console.log(count);
     }, 300);
-  }
+  };
 
 
  
